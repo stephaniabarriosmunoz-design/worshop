560a0f6a3463f960298c0d074a230338a7ac580b
recursos <you@example.com>
2026-09-12 16:48:51 -0500

    funcion de multiplicacion añadida

--- py.py
+++ py.py
@@ -1,5 +1,1 @@
-def sum( a, b):
-    return a + b
 
-result = sum(5, 3)
-print(result)
---
+++ py2.py
@@ -0,0 +1,18 @@
+def sum( a, b):
+    return a + b
+
+result = sum(5, 3)
+print(result) 
+
+def resta( a, b):
+    return a - b
+
+result = resta(5, 3)
+print(result)
+
+
+def multiplicacion( a, b):
+    return a * b    
+
+result = multiplicacion(5, 3)
+print(result)
